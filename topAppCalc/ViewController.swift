d383780a041745c742e5a0c3b270ea6438c250a5
EvhenAksionov <aksionov.evhen@gmail.com>
2023-07-23 19:25:45 +0300

    guard + validation

--- topAppCalc/ViewController.swift
+++ topAppCalc/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet private weak var aTextField: UITextField!
     @IBOutlet private weak var bTextField: UITextField!
@@ -16,83 +16,85 @@ class ViewController: UIViewController {
     
     @IBOutlet private weak var resultLabel: UILabel!
     
-    
-    
+    // MARK: func validation
+    func textFieldIsEmpty (message: String) {
+        let TextFieldIsEmptyAlert = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        TextFieldIsEmptyAlert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
+        self.present(TextFieldIsEmptyAlert, animated: true)
+    }
     
     
     @IBAction func calculate() {
         
         guard let aText = aTextField.text,
               let bText = bTextField.text,
-              let cText = cTextField.text,
-              let a = Float(aText),
+              let cText = cTextField.text
+              else {return}
+
+        guard !aText.isEmpty else {
+            textFieldIsEmpty(message: "a is empty")
+            return}
+        guard !bText.isEmpty else {
+            textFieldIsEmpty(message: "b is empty")
+            return}
+        guard !cText.isEmpty else {
+            textFieldIsEmpty(message: "c is empty")
+            return}
+        
+        guard let a = Float(aText),
               let b = Float(bText),
-              let c = Float(cText) else {return}
+              let c = Float(cText)
+                else {return}
         
-        
-        
-        if aText.isEmpty {
-            let aTextIsEmptyAlert = UIAlertController(title: "", message: "Не вказано значення a", preferredStyle: .alert)
-            aTextIsEmptyAlert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
-            self.present(aTextIsEmptyAlert, animated: true)
-        } else if bText.isEmpty{
-            let bTextIsEmptyAlert = UIAlertController(title: "", message: "Не вказано значення b", preferredStyle: .alert)
-            bTextIsEmptyAlert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
-            self.present(bTextIsEmptyAlert, animated: true)
-        } else if cText.isEmpty {
-            let cTextIsEmptyAlert = UIAlertController(title: "", message: "Не вказано значення c", preferredStyle: .alert)
-            cTextIsEmptyAlert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
-            self.present(cTextIsEmptyAlert, animated: true)
+        guard a != .zero else {
+            textFieldIsEmpty(message: "a can`t = 0")
+            return
         }
         
-        let d = pow(b,2) - (4 * a * c)
-        
+            let d = pow(b,2) - (4 * a * c)
+            
         enum Result {
-            case noSqrt
-            case oneSqrt
-            case twoSqrt
-        }
+                case noRoots
+                case oneRoot
+                case twoRoots
+            }
         
-        var calculate = Result.noSqrt
+        var calculate1 = Result.noRoots
         
-        if d < .zero{
-            calculate = Result.noSqrt
-        } else if d == .zero {
-            calculate = Result.oneSqrt
-        } else if d > .zero{
-            calculate = Result.twoSqrt
-        }
         
-        switch calculate{
-        case.noSqrt:
+        
+        switch calculate1{
+        case .noRoots:
             resultLabel.text = "дійсних коренів немає"
-        case .oneSqrt:
+        case .oneRoot:
             let x = -b / (2 * a)
             resultLabel.text = "x = \(x)"
-        case .twoSqrt:
+        case .twoRoots:
             let sqrtDicriminant = sqrt(d)
             let x1 = (-b + sqrtDicriminant) / (2 * a)
             let x2 = (-b - (sqrtDicriminant)) / (2 * a)
             resultLabel.text = "x1 = \(round((1000 * x1) / 1000)), x2 = \(round((1000 * x2) / 1000))"
         }
         
-        
-    
-  
-//
-//        if d < .zero {
-//            resultLabel.text = "дійсних коренів немає"
-//        } else if d == .zero {
-//            let x = -b / (2 * a)
-//
-//            resultLabel.text = "x = \(x)"
-//        } else if d > .zero{
-//            let sqrtDicriminant = sqrt(d)
-//            let x1 = (-b + sqrtDicriminant) / (2 * a)
-//            let x2 = (-b - (sqrtDicriminant)) / (2 * a)
-//
-//            resultLabel.text = "x1 = \(round((1000 * x1) / 1000)), x2 = \(round((1000 * x2) / 1000))"
-//        }
+        guard d >= .zero else {
+            calculate1 = Result.noRoots
+            return
+        }
+        guard d > 0 || d < 0 else {
+            calculate1 = Result.oneRoot
+            return
+        }
+        guard d <= 0 else {
+            calculate1 = Result.twoRoots
+            return
+        }
     }
 }
+        
+
+        
+  
+
+    
+
 
